5b795542a88b85cc64fcdf135075030d0a21d8bd
Crutheo <52221221+Crutheo@users.noreply.github.com>
2019-07-01 17:15:07 -0500

    Update Collision.py

--- Collision.py
+++ Collision.py
@@ -1,50 +1,49 @@
-def check_collission(head, segment, pos, score, pen, text,dict):
+def check_collission(dict, parts):
 
     #    Chequea si se choca contra el muro
-    if head.xcor() > 280 or head.ycor() > 280 or head.xcor() < -280 or head.ycor() < -280:
-        head.goto(pos, 0)
-        head.direction = "stop"
+    if parts["Head"].xcor() > 280 or parts["Head"].ycor() > 280 or parts["Head"].xcor() < -280 or \
+            parts["Head"].ycor() < -280:
+        parts["Head"].goto(parts["InitialPos"], 0)
+        parts["Head"].direction = "stop"
         dict["Score"] = 0
-        score = score - score
-        text = "Score: %s" % score
-        pen.clear()
-        pen.write(text, False, align="Left", font=("Arial", 14, "normal"))
+        dict["Score"] = 0
+        parts["Text"] = "Score: %s" % dict["Score"]
+        parts["Pen"].clear()
+        parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
 
-        for red_snake_body in segment:
-            red_snake_body.goto(-1000, -1000)
+        for snake_body in parts["Segment"]:
+            snake_body.goto(-1000, -1000)
 
-        segment.clear()
+        parts["Segment"].clear()
 
     #   Chequea si se choca consigo mismo
-    for body in segment:
-        if body.distance(head) < 20:
-            head.goto(pos, 0)
-            head.direction = "stop"
+    for body in parts["Segment"]:
+        if body.distance(parts["Head"]) < 20:
+            parts["Head"].goto(parts["InitialPos"], 0)
+            parts["Head"].direction = "stop"
             dict["Score"] = 0
-            score = 0
-            text = "Score: %s" % score
-            pen.clear()
-            pen.write(text, False, align="Left", font=("Arial", 14, "normal"))
+            parts["Text"] = "Score: %s" % dict["Score"]
+            parts["Pen"].clear()
+            parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
 
-            for body in segment:
+            for body in parts["Segment"]:
                 body.goto(-1000, -1000)
 
-            segment.clear()
+            parts["Segment"].clear()
 
 
-def check_snake_collision(head, segment, opponent_segment, pos, score, pen, text, dict):
-    for body in opponent_segment:
-        if body.distance(head) < 20:
-            head.goto(pos, 0)
-            head.direction = "stop"
+def check_snake_collision(dict, parts, enemyParts):
+    for body in enemyParts["Segment"]:
+        if body.distance(parts["Head"]) < 20:
+            parts["Head"].goto(parts["InitialPos"], 0)
+            parts["Head"].direction = "stop"
+
             dict["Score"] = 0
-            score = 0
-            text = "Score: %s" % score
-            pen.clear()
-            pen.write(text, False, align="Left", font=("Arial", 14, "normal"))
+            parts["Text"] = "Score: %s" % dict["Score"]
+            parts["Pen"].clear()
+            parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
 
-            for body in segment:
+            for body in parts["Segment"]:
                 body.goto(-1000, -1000)
 
-            segment.clear()
-
+            parts["Segment"].clear()
