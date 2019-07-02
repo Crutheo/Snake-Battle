2ecb7122b7d1408fc23084be43e7abbdb190f5b4
Crutheo <52221221+Crutheo@users.noreply.github.com>
2019-07-02 00:57:40 -0500

    Update Collision.py

--- Collision.py
+++ Collision.py
@@ -1,49 +1,52 @@
-def check_collission(dict, parts):
+import random
+import turtle
+
+def check_collission(dict, parts, food, enemyParts):
 
     #    Chequea si se choca contra el muro
     if parts["Head"].xcor() > 280 or parts["Head"].ycor() > 280 or parts["Head"].xcor() < -280 or \
             parts["Head"].ycor() < -280:
-        parts["Head"].goto(parts["InitialPos"], 0)
-        parts["Head"].direction = "stop"
-        dict["Score"] = 0
-        dict["Score"] = 0
-        parts["Text"] = "Score: %s" % dict["Score"]
-        parts["Pen"].clear()
-        parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
-
-        for snake_body in parts["Segment"]:
-            snake_body.goto(-1000, -1000)
-
-        parts["Segment"].clear()
+        clear(dict,parts)
 
     #   Chequea si se choca consigo mismo
     for body in parts["Segment"]:
         if body.distance(parts["Head"]) < 20:
-            parts["Head"].goto(parts["InitialPos"], 0)
-            parts["Head"].direction = "stop"
-            dict["Score"] = 0
-            parts["Text"] = "Score: %s" % dict["Score"]
-            parts["Pen"].clear()
-            parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
+            clear(dict,parts)
 
-            for body in parts["Segment"]:
-                body.goto(-1000, -1000)
-
-            parts["Segment"].clear()
-
-
-def check_snake_collision(dict, parts, enemyParts):
+    #   Chequea si se choca con otra serpiente
     for body in enemyParts["Segment"]:
         if body.distance(parts["Head"]) < 20:
-            parts["Head"].goto(parts["InitialPos"], 0)
-            parts["Head"].direction = "stop"
+            clear(dict,parts)
 
-            dict["Score"] = 0
-            parts["Text"] = "Score: %s" % dict["Score"]
-            parts["Pen"].clear()
-            parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
+    # Chequear si se choca con la comida
+    if parts["Head"].distance(food) < 20:
+        x_move = random.randint(-14, 14) * 20
+        y_move = random.randint(-14, 13) * 20
+        food.goto(x_move, y_move)
 
-            for body in parts["Segment"]:
-                body.goto(-1000, -1000)
+        dict["Score"] = dict["Score"] + 1
+        parts["Text"] = "Score: %s" % dict["Score"]
+        parts["Pen"].clear()
+        parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
 
-            parts["Segment"].clear()
+        body = turtle.Turtle()
+        body.penup()
+        body.speed(0)
+        body.color("dark "+dict["Color"])
+        body.shape("square")
+        parts["Segment"].append(body)
+
+
+def clear(dict, parts):
+    parts["Head"].goto(parts["InitialPos"], 0)
+    parts["Head"].direction = "stop"
+
+    dict["Score"] = 0
+    parts["Text"] = "Score: %s" % dict["Score"]
+    parts["Pen"].clear()
+    parts["Pen"].write(parts["Text"], False, align="Left", font=("Arial", 14, "normal"))
+
+    for body in parts["Segment"]:
+        body.goto(-1000, -1000)
+
+    parts["Segment"].clear()
